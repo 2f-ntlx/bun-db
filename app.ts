26e06c0f5d8bfaf8eb246e8a31c41a3ced7f14dd
ntlx <anmail@ukr.net>
2023-01-27 21:09:54 +0200

    add sqlite

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import {Database} from "bun:sqlite"
+
+console.log("begin bun app")
+const db = Database.open('test.db')
+// const db = new Database();
+// db.run(
+//   "CREATE TABLE IF NOT EXISTS foo (id INTEGER PRIMARY KEY AUTOINCREMENT, greeting TEXT)",
+// );
+// db.run("INSERT INTO foo (greeting) VALUES (?)", "Welcome to bun!");
+// db.run("INSERT INTO foo (greeting) VALUES (?)", "Hello World!");
+
+console.log("end bun app")
